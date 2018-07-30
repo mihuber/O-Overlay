c0c4823bb5d5f51e961ac9ccf262bb5d383c510d
Michael Huber <huber.michael@virology.uzh.ch>
2018-07-30 23:06:58 +0200

    Create overlay_pdf.sh

---
+++ overlay_pdf.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+bkgr=$(ls | grep .All.pdf)
+list=$(ls | grep .Purple.pdf)
+output=$(echo $bkgr | sed s/All.pdf/Overlay.pdf/)
+
+echo $bkgr
+echo $list
+
+array=($list)
+n=$(expr ${#array[@]} - 1)
+
+temp_pdf_in=${array[0]}
+for i in $(seq 1 $n); do
+	echo $i ${array[$i]}
+	pdftk ${array[$i]} background $temp_pdf_in output temp_pdf_${i}.pdf
+    temp_pdf_in=$(echo temp_pdf_${i}.pdf)
+done
+
+pdftk $temp_pdf_in background $bkgr output $output
+rm temp_pdf_*.pdf
