383692c8e40d32faf296fe24d08fadd08ed68060
Michael Huber <huber.michael@virology.uzh.ch>
2018-10-10 09:44:40 +0200

    Update overlay_pdf.sh

--- overlay_pdf.sh
+++ overlay_pdf.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 bkgr=$(ls | grep .All.pdf)
-list=$(ls | grep .Purple.pdf)
+list=$(ls | grep .pdf | grep -v All.pdf | grep -v Overlay.pdf)
 output=$(echo $bkgr | sed s/All.pdf/Overlay.pdf/)
 
 echo $bkgr
